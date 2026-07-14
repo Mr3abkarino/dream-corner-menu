c150e6cd976b72316b00f6a663f71a447142b1ac
Mr3abkarino <mr3abkarinosoft@gmail.com>
2026-07-14 06:54:33 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// ربط تطبيق ريأكت بالعنصر ذو المعرف 'root' الموجود في ملف index.html وإطلاقه
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
-
